4e51e415e402d5098f6d53a9be028348bbe0ba10
Laila2005 <laila.mf2005@gmail.com>
2025-04-24 13:20:18 +0200

    task6

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,3 @@
+def sum_mixed_list(mxd_lst: list[float, int]) -> float:
+    """function that takes a list mxd_lst of integers and floats and returns their sum as a float."""
+    return sum(mxd_lst)
