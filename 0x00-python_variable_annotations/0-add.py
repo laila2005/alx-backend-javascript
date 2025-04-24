5a7ccdcb73c3324309638e46dd844250d44a069d
laila2005 <laila.mf2005@gmail.com>
2025-04-24 11:52:27 +0000

    adjusting task2

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""Function that takes a float a and a float b and returns their sum as a float."""
+
+
 def add(a: float, b: float) -> float:
-    """Function that takes a float a and a float b and returns their sum as a float."""
-    return a + b
+    """add two floats"""
+    return (a + b)
