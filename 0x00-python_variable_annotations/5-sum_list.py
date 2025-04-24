801c902f6ea5e0f532194d655fe3f0aa1ab90214
Laila2005 <laila.mf2005@gmail.com>
2025-04-24 12:51:31 +0200

    task5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,5 @@
+from typing import List
+
+def sum_list(input_list: list[float]) -> float:
+             """function that takes a list of floats and returns their sum as a float"""
+             return sum(input_list)
