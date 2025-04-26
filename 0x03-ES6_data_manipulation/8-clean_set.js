905e22c102d2e6568257ba4018c7b60c0e80c441
Laila2005 <laila.mf2005@gmail.com>
2025-04-26 20:34:17 +0300

    task8

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,11 @@
+export default function cleanSet(set, startString) {
+  if (!startString || typeof startString !== 'string') {
+    return '';
+  }
+
+  const filtered = [...set]
+    .filter((value) => value.startsWith(startString))
+    .map((value) => value.slice(startString.length));
+
+  return filtered.join('-');
+}
